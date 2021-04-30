ce7797dc31d6b7990455ffa98774c018151efb08
guilherme <guilherme.alves96@hotmail.com>
2021-04-30 00:19:07 -0300

    Criando banco de dados - CRUD

--- minhasaude/settings.py
+++ minhasaude/settings.py
@@ -60,8 +60,11 @@ WSGI_APPLICATION = 'minhasaude.wsgi.application'
 # Database
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'alimentos',
+        'USER': 'postgres',
+        'PASSWORD': '529440',
+        'HOST': 'localhost'
     }
 }
 
--- msapp/models.py
+++ msapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from datetime import datetime as DT
 
-# Create your models here.
+class Produtos(models.Model):
+
+    quantidade = models.IntegerField()
+    nome = models.CharField(max_length=200)
+    qtde_carboidrato = models.TextField()
+    # mlg_insulina = models.CharField(max_length=200)
+    data_atual = models.DateTimeField(default=DT.now, blank=True)
